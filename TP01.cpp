4a2e14385d8652a5ea7c03c54cfa85bb9babe653
lucas Amaral <lucas.amaral.conta@gmail.com>
2023-02-28 12:52:52 -0300

    Adicionar arquivos de projeto.

---
+++ TP01.cpp
@@ -0,0 +1,42 @@
+// TP01.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string nome = "";
+	int p1 = 0;
+	int p2 = 0;
+	int media = 0;
+
+   cout << "Digite o nome do aluno\n";
+   cin >> nome;
+   cout << "Digite a nota da primeira prova\n";
+   cin >> p1;
+   cout << "Digite a nota da segunda prova\n";
+   cin >> p2;
+
+   media = (p1 + p2) / 2;
+
+   if (media >= 5) {
+	   cout << "O aluno " + nome + " esta aprovado.";
+   }
+
+   else {
+	   cout << "O aluno " + nome + " esta reprovado.";
+   }
+   
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
